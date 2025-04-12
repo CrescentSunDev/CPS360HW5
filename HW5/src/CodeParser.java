27eaf0a5c8f46e625bf2c955cfbf897909280f15
calla3mm <calla3mm@cmich.edu>
2025-04-11 20:12:28 -0700

    hashcreator implemented

--- HW5/src/CodeParser.java
+++ HW5/src/CodeParser.java
@@ -1,8 +1,26 @@
+// HashCreator.java
+// Contributor(s):
+// Meagan Callahan - created outline of class and class file
+// Desc: this class takes in a line of code and parses it into the correct sections,
+// comp, dest, and jump for c-instructions or addr for A-instructions. These sections can
+// then be accessed and translated
+
 public class CodeParser {
     private String comp, dest = "null", jump = "null", addr;
-// parses a line of code into the correct sections, ie comp, dest, and jump for c-instructions or addr for A-instructions
+
+// parses a line of code, represented by a String s, into the correct sections and returns true
+// if the parse is successful and false if it cannot be parsed
     public boolean parse(String s) {
-        return true;
+        s = s.trim();
+
+
+        if(!s.isEmpty()){
+            if(s.charAt(0) != '/' && s.charAt(1) != '/' ){
+
+                return true;
+            }
+        }
+        return false; // returns false if the line of code is whitespace or a comment
     }
 
     private void parseJump(String s) {
--- HW5/src/HashCreator.java
+++ HW5/src/HashCreator.java
@@ -1,19 +1,53 @@
 // HashCreator.java
 // Contributor(s):
-// Meagan Callahan - created outline of class and class file
+// Meagan Callahan - created outline of class and class file,
 // Desc: this class takes in the values of comp.txt, dest.txt, jump.txt, and symbols.txt and
 // creates a hashmap containing the values for easier translating
 
-
 import java.util.Hashtable;
+import java.util.Scanner;
 
 public class HashCreator {
+    //private scanner used in initialize(String filename) method
+    private Scanner sc;
     //universal hashtables
     private final Hashtable<String, String> comp = new Hashtable<>(), dest = new Hashtable<>(), jump = new Hashtable<>(), symbol = new Hashtable<>();
 
     public HashCreator() {
-        //scans tables and sets up hashtables
+        //calls method to scan tables and fill hashtables
+        initialize("HW/comp.txt");
+        initialize("HW/dest.txt");
+        initialize("HW/jump.txt");
+        initialize("HW/symbol.txt");
+    }
 
+    // takes in a variable file name and identifies whether the file is the comp table,
+    // dest table, jump table, or symbol table provided in the text files
+    private void initialize(String fileName) {
+        String line;
+        sc = new Scanner(fileName);
+
+        //checks which file is being initialized and fills the respective hashtable
+        while(sc.hasNextLine()){
+            // properly formats line
+            line = sc.nextLine();
+            line = line.trim();
+            String[] pair = line.split("_");
+            if(fileName.contains("comp.txt")) {
+                comp.put(pair[0], pair[1]);
+            } else if(fileName.contains("dest.txt")){
+                dest.put(pair[0], pair[1]);
+            } else if(fileName.contains("jump.txt")){
+                jump.put(pair[0], pair[1]);
+            } else if(fileName.contains("symbol.txt")){
+                symbol.put(pair[0], pair[1]);
+            }
+        }
+
+    }
+
+    public void addVariable(String key, String val) {
+        comp.put(key, val);
     }
 
     //getters to allow access to translations from hashtables
