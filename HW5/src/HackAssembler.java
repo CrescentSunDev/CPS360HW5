cba304f44982bd750b2ba0224444b91392d0b4eb
Liaposting <justin.deines11@gmail.com>
2025-04-11 23:09:55 -0400

    Refactored main to HackAssembler, added a skeleton to it

---
+++ HW5/src/HackAssembler.java
@@ -0,0 +1,34 @@
+//Assignment: HW5, Hack assembler
+//Group members: Justin Deines and Meagan Callahan
+
+public class HackAssembler {
+    //symbol table object
+    //int current line, describes current line in file I/O
+    //parser object
+
+    //constructor, set symbol table and current line to 0
+
+    //first pass method
+    public void pass1(){
+
+    }
+
+    //second pass/translate method
+    public void pass2(){
+
+    }
+
+    //binary padding method (not sure if necessary but i feel it would be smart to add just in case)
+    public String padBinary(String unpadded){
+        String padded = "";
+        return padded + unpadded;
+    }
+    public static void main(String[] args) {
+        //call parser with filename from args[0]
+        //initiate assembler
+        //call first pass
+        //call translate pass
+
+
+    }
+}
